feba18af87a1edb6bdf3c81becfca205f277268e
xputnikx <xputnikx@gmail.com>
2015-06-20 22:25:42 +0300

    vc

---
+++ SMG Times/DetailsViewController.swift
@@ -0,0 +1,27 @@
+//
+//  DetailsViewController.swift
+//  SMG Times
+//
+//  Created by Vladimir Hudnitsky on 6/20/15.
+//  Copyright (c) 2015 lumenela. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DetailsViewController: UIViewController{
+
+    @IBOutlet weak var detailedTable: UITableView!
+    
+//    override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+//        return 1
+//    }
+//    
+//    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        if let list = self.records {
+//            return list.count
+//        }
+//        return 0
+//    }
+    
+}
